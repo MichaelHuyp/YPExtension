abfbf64a053a9bf3dcdb6e2da2a075160771ae8e
MichaelHuyp <86812684@qq.com>
2015-08-18 13:07:24 +0800

    YPExtension 1.0 初步搭建
    Foundation+YPLog.m （数组、字典）本地化打印Log
    pchFile.pch YP专属头文件
    UIScrollView+YPExtension 给ScrollView添加常用属性拓展
    UIView+YPExtension 给UIView添加常用属性拓展

---
+++ UIView+YPExtension.h
@@ -0,0 +1,20 @@
+//
+//  UIView+YPExtension.h
+//  YPExtension
+//
+//  Created by 胡云鹏 on 15/8/18.
+//  Copyright (c) 2015年 MichaelPPP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (YPExtension)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@end
